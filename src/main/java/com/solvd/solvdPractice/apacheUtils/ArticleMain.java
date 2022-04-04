a7793a0b7a16cd3169ce36aad1cedfaa6e64c3d6
mja123 <matias.aguilar.sansoni@gmail.com>
2022-04-04 08:04:52 -0300

    System variables added

--- src/main/java/com/solvd/solvdPractice/apacheUtils/ArticleMain.java
+++ src/main/java/com/solvd/solvdPractice/apacheUtils/ArticleMain.java
@@ -12,23 +12,14 @@ import java.util.*;
 
 public class ArticleMain {
     private static final Logger LOGGER = LogManager.getLogger(ArticleMain.class);
-    /*TODO:
-    - create a FIleManage class and split this code in 2 methods
-    - configure the log4j2.xml with the different status and appenders (https://howtodoinjava.com/log4j2/multiple-appenders/)
-    - use environment variables to paths.
-     */
+
     public static void main(String[] args) {
-        final File TEXT = new File("C:\\Users\\Usuario\\OneDrive\\Desktop\\solvdPractice\\src\\main\\resources\\article.txt");
+        final File TEXT = new File(System.getenv("ARTICLE"));
 
         HashMap<String, Integer> wordOccurrences = new HashMap<>();
         String[] words = null;
         String text = null;
-        LOGGER.info("Info");
-        LOGGER.fatal("fatal");
-        LOGGER.debug("debug");
-        LOGGER.trace("trace");
-        LOGGER.warn("warn");
-        LOGGER.error("error");
+
         try {
             text = FileUtils.readFileToString(TEXT, StandardCharsets.UTF_8);
 
@@ -45,7 +36,7 @@ public class ArticleMain {
             }
         }
         try {
-            FileWriter answer = new FileWriter("C:\\Users\\Usuario\\OneDrive\\Desktop\\solvdPractice\\src\\main\\java\\com\\solvd\\solvdPractice\\apacheUtils\\answer.txt");
+            FileWriter answer = new FileWriter("src/main/java/com/solvd/solvdPractice/apacheUtils/answer.txt");
             for (String word : wordOccurrences.keySet()) {
                 answer.write(word + " - " + wordOccurrences.get(word) + "\n");
             }
